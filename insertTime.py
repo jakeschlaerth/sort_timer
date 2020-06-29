b6a78ce3a7e5ae2fb7b9ada4615ec85c63df85ec
jakeschlaerth <schlaerj@oregonstate.edu>
2020-06-28 21:47:39 -0400

    Initial commit

---
+++ insertTime.py
@@ -0,0 +1,51 @@
+# Author: Jacob Schlaerth
+# Date: 06/28/2020
+import random
+import time
+
+
+def time_decorator(func):
+    """decorator function that returns the time needed for a passed sorting algorithm to complete"""
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(run_time, end="")
+        return run_time
+    return wrapper
+
+
+def randList(n):
+    """
+    fills an list of length n with random integers between 0 and 10000
+    :param n: length of list
+    :return: list
+    """
+    rand_list = []
+    for i in range(0, n):
+        rand_list.append(random.randint(0, 10000))
+    return rand_list
+
+
+@time_decorator
+def insertionSort(num_list):
+    """
+    sort a list of numbers via insertion sorting algorithm
+    :param num_list: num_list[0] = how many numbers to sort
+    :return: None, num_list is mutable
+    """
+    for ind in range(1, len(num_list)):
+        current = num_list[ind]
+        j = ind - 1
+        while j >= 0 and current > num_list[j]:
+            num_list[j + 1] = num_list[j]
+            j -= 1
+        num_list[j + 1] = current
+
+
+for i in range(5000, 25000, 5000):
+    random_list = randList(i)
+    print("array of length", i, "sorted in ", end="")
+    insertionSort(random_list)
+    print(" seconds via insertion sort.")
---
+++ mergeTime.py
@@ -0,0 +1,77 @@
+# Author: Jacob Schlaerth
+# Date: 06/28/2020
+import random
+import time
+
+
+def time_decorator(func):
+    """decorator function that returns the time needed for a passed sorting algorithm to complete"""
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(run_time, end="")
+        return run_time
+    return wrapper
+
+
+def randList(n):
+    """
+    fills an list of length n with random integers between 0 and 10000
+    :param n: length of list
+    :return: list
+    """
+    rand_list = []
+    for i in range(0, n):
+        rand_list.append(random.randint(0, 10000))
+    return rand_list
+
+
+def mergeSort(num_list):
+    """
+    sorts a list via merge sorting algorithm
+    :param num_list: num_list[0] = number of elements to be sorted
+    :return: none, num_list is mutable
+    """
+    if len(num_list) > 1:
+        middle = len(num_list) // 2  # integer divide to find middle
+        left = num_list[:middle]  # slice list into right and left halves
+        right = num_list[middle:]
+
+        mergeSort(left)  # recursive call
+        mergeSort(right)  # recursive call
+
+        i = 0  # init variables
+        j = 0
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] > right[j]:
+                num_list[k] = left[i]
+                i += 1
+            else:
+                num_list[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            num_list[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            num_list[k] = right[j]
+            j += 1
+            k += 1
+
+
+for i in range(5000, 25000, 5000):
+    random_list = randList(i)
+    print("array of length", i, "sorted in ", end="")
+    start_time = time.perf_counter()
+    mergeSort(random_list)
+    end_time = time.perf_counter()
+    run_time = end_time - start_time
+    print(run_time, end="")
+    print(" seconds via merge sort.")
